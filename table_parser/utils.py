14686d6b5bff36a6a206b9862e3215256fb94adc
nazarii <ntehta96@gmail.com>
2021-04-26 18:23:10 +0300

    Optional initial image drawing

--- table_parser/utils.py
+++ table_parser/utils.py
@@ -989,7 +989,7 @@ def derotate_angle(img, left=-3, right=3, n_iter=4, resize_k=0.3):
     return best_angle
 
 
-def draw_lines(image, table, linewidth=5, alpha=0.5):
+def draw_lines(image, table, linewidth=5, alpha=0.5, draw_image=True):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     for row in table.cells:
@@ -1002,7 +1002,8 @@ def draw_lines(image, table, linewidth=5, alpha=0.5):
                 ax.plot([cell.lt[0], cell.rt[0]], [cell.lt[1], cell.rt[1]], linewidth=linewidth, alpha=alpha)
             if cell.bottom:
                 ax.plot([cell.lb[0], cell.rb[0]], [cell.lb[1], cell.rb[1]], linewidth=linewidth, alpha=alpha)
-    plt.imshow(image[::-1], origin='lower')
+    if draw_image:
+        plt.imshow(image[::-1], origin='lower')
     return fig
 
 
